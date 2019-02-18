5f4c40ecb3cb6be81ae0fe140c7b9566af41bb82
CaretCake <caretcake@outlook.com>
2019-02-18 16:33:01 -0500

    Refactoring

--- DataQualityReportGenerator.java
+++ DataQualityReportGenerator.java
@@ -6,11 +6,14 @@ public class DataQualityReportGenerator {
 	public static void main(String[] args) throws Exception {
 
 		DataProcessor dataProcessor = new DataProcessor();
+		DataPrinter dataPrinter = new DataPrinter();
 
 		//TODO: un-hardcode the filename
 		dataProcessor.setInputFilename("lakes.arff");
+		dataProcessor.setDataPrinter(dataPrinter);
 		dataProcessor.parseDataFromArffFile();
-		dataProcessor.generateDataQualityReport("lakesDQR.csv");
+		dataProcessor.generateDataQualityReport();
+		dataPrinter.printDataQualityReport("lakesDQR.csv");
 
 	}
 }
