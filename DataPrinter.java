11b006ad78dda2ae97c3e6a2efb46b0076c4373f
CaretCake <caretcake@outlook.com>
2019-02-17 22:44:24 -0500

    Added methods for printing numeric and categorical headers for DQR

--- DataPrinter.java
+++ DataPrinter.java
@@ -1,9 +1,24 @@
+import java.io.*;
+
 public class DataPrinter {
 	private String NUMERIC_HEADER = "Numeric feature,Percent missing,Cardinality,Minimum,1st Quartile,Mean,Median,3rd Quartile,Maximium,Standard Deviation";
 	private String CATEGORICAL_HEADER = "Categorical feature,Percent missing,Cardinality,Mode,Mode frequency,Mode percent,Second mode,Second mode frequency,Second mode percent";
+	private PrintWriter out;
 
-	public void printTotalInstances (int numberOfInstances, PrintWriter out) {
+	public void setPrintWriter(PrintWriter printWriter) {
+		out = printWriter;
+	}
+
+	public void printTotalInstances (int numberOfInstances) {
 		out.println("Instances\n" + numberOfInstances + "\n");
+	}
+
+	public void printNumericHeader() {
+		out.println(NUMERIC_HEADER);
+	}
+
+	public void printCategoricalHeader() {
+		out.println(CATEGORICAL_HEADER);
 	}
 
 }
