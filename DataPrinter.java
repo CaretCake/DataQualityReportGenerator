fb437846c8b7e4434648699589217e59783ac8b5
CaretCake <caretcake@outlook.com>
2019-02-18 21:33:57 -0500

    Refactoring and cleaning up code.

--- DataPrinter.java
+++ DataPrinter.java
@@ -2,24 +2,12 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class DataPrinter {
-	private String NUMERIC_HEADER = "Numeric feature,Cardinality,Minimum,1st Quartile,Median,3rd Quartile,Maximium,Mean,Standard Deviation";
-	private String CATEGORICAL_HEADER = "Categorical feature,Cardinality,Mode,Mode frequency,Mode percent,Second mode,Second mode frequency,Second mode percent";
+	static final String NUMERIC_HEADER = "Numeric feature,Cardinality,Minimum,1st Quartile,Median,3rd Quartile,Maximium,Mean,Standard Deviation";
+	static final String CATEGORICAL_HEADER = "Categorical feature,Cardinality,Mode,Mode frequency,Mode percent,Second mode,Second mode frequency,Second mode percent";
 	private ArrayList<String> numericFeatureData = new ArrayList<String>();
 	private ArrayList<String> categoricalFeatureData = new ArrayList<String>();
 	private int totalNumberOfInstances;
 	private PrintWriter out;
-
-	public void addNumericFeatureData(String commaSeparatedData) {
-		this.numericFeatureData.add(commaSeparatedData);
-	}
-
-	public void addCategoricalFeatureData(String commaSeparatedData) {
-		this.categoricalFeatureData.add(commaSeparatedData);
-	}
-
-	public void setTotalNumberOfInstances(int numberOfInstances) {
-		totalNumberOfInstances = numberOfInstances;
-	}
 
 	private void printTotalInstances() {
 		out.println("Instances\n" + totalNumberOfInstances);
@@ -47,6 +35,18 @@ public class DataPrinter {
 
 	private void printBlankLine() {
 		out.println();
+	}
+
+	public void addNumericFeatureData(String commaSeparatedData) {
+		this.numericFeatureData.add(commaSeparatedData);
+	}
+
+	public void addCategoricalFeatureData(String commaSeparatedData) {
+		this.categoricalFeatureData.add(commaSeparatedData);
+	}
+
+	public void setTotalNumberOfInstances(int numberOfInstances) {
+		totalNumberOfInstances = numberOfInstances;
 	}
 
 	public void printDataQualityReport(String outputFilename) throws Exception {
--- DataProcessor.java
+++ DataProcessor.java
@@ -11,16 +11,26 @@ public class DataProcessor {
 	private ArrayList<ArrayList<Double>> dataInstances = new ArrayList<ArrayList<Double>>();
 	private HashMap<String, ArrayList<String>> categoricalFeatureOptions = new HashMap<String, ArrayList<String>>();
 
-	public void setInputFilename(String filename) {
-		this.inputFilename = filename;
+	private void addFeature(String feature) {
+		this.features.add(feature);
 	}
 
-	public void setDataPrinter(DataPrinter dataPrinter) {
-		this.dataPrinter = dataPrinter;
+	private void addCategoricalFeatureOptions(String feature, String featureOptionsString) {
+		featureOptionsString = featureOptionsString.substring(1, featureOptionsString.length()-1);
+		ArrayList<String> featureOptions = new ArrayList(Arrays.asList(featureOptionsString.split(",")));
+		this.categoricalFeatureOptions.put(feature, featureOptions);
 	}
 
 	private int getIndexOfValueForCategoricalFeature(String categoricalFeatureName, String value) {
 		return categoricalFeatureOptions.get(categoricalFeatureName).indexOf(value);
+	}
+
+	private int getCardinalityOfFeatureAtIndex(int featureIndex) {
+		HashMap<Double, String> cardinalityMap =  new HashMap<Double, String>();
+		for (ArrayList<Double> dataInstance : dataInstances) {
+			cardinalityMap.put(dataInstance.get(featureIndex), "");
+		}
+		return cardinalityMap.size();
 	}
 
 	private String getNameFromIndexForCategoricalFeature(String categoricalFeatureName, int indexValue) {
@@ -33,99 +43,6 @@ public class DataProcessor {
 
 	private String getFeatureType(int index) {
 		return  (this.categoricalFeatureOptions.get(getFeatureName(index)) == null) ? "numeric" : "categorical";
-	}
-
-	private void addFeature(String feature) {
-		this.features.add(feature);
-	}
-
-	private void addCategoricalFeatureOptions(String feature, String featureOptionsString) {
-		featureOptionsString = featureOptionsString.substring(1, featureOptionsString.length()-1);
-		ArrayList<String> featureOptions = new ArrayList(Arrays.asList(featureOptionsString.split(",")));
-		this.categoricalFeatureOptions.put(feature, featureOptions);
-	}
-
-	public void addDataInstance(String dataInstance) {
-		String[] data = dataInstance.split(",");
-		ArrayList<Double> processedDataInstance = new ArrayList<Double>();
-		double processedValue = 0;
-		for (int i = 0; i < data.length; i++) {
-			if (getFeatureType(i) == "categorical") {
-				processedValue = getIndexOfValueForCategoricalFeature(getFeatureName(i), data[i]);
-				processedDataInstance.add(processedValue);
-			}
-			else {
-				processedValue = Double.parseDouble(data[i]);
-				processedDataInstance.add(processedValue);
-			}
-		}
-		this.dataInstances.add(processedDataInstance);
-	}
-
-	public void parseDataFromArffFile() throws Exception {
-		Scanner in = new Scanner(new File(inputFilename), "UTF-8");
-		in.nextLine();
-
-		while(in.hasNextLine()) {
-			String line = in.nextLine().trim();
-			if (line.length() > 0 && line.charAt(0) != '%' && !line.contains("@data") && !line.contains("@relation")) {
-				if (line.contains("@attribute")) {
-					String[] lineSplitOnSpace = line.split(" ");
-					switch (lineSplitOnSpace[2].trim()) {
-						case "numeric":
-							this.addFeature(lineSplitOnSpace[1].trim());
-							break;
-						default:
-							this.addFeature(lineSplitOnSpace[1].trim());
-							this.addCategoricalFeatureOptions(lineSplitOnSpace[1].trim(), lineSplitOnSpace[2].trim());
-							break;
-					}
-				}
-				else {
-					this.addDataInstance(line);
-				}
-			}
-		}
-	}
-
-	public void generateDataQualityReport() {
-		for (int i = 0; i < this.features.size(); i++) {
-			if (getFeatureType(i) == "numeric") {
-				dataPrinter.addNumericFeatureData(processNumericFeatureData(i));
-			}
-			else if (getFeatureType(i) == "categorical") {
-				dataPrinter.addCategoricalFeatureData(processCategoricalFeatureData(i));
-			}
-		}
-
-		dataPrinter.setTotalNumberOfInstances(this.dataInstances.size());
-	}
-
-	private String processNumericFeatureData(int featureIndex) {
-		String featureName = getFeatureName(featureIndex);
-		String featureData = featureName+",";
-		featureData += getCardinalityOfFeatureAtIndex(featureIndex) + ",";
-		featureData += getQuartilesOfFeatureAtIndex(featureIndex) + ",";
-		featureData += getMeanAndStandardDeviationOfFeatureAtIndex(featureIndex);
-
-		return featureData;
-	}
-
-	private String processCategoricalFeatureData(int featureIndex) {
-		String featureName = getFeatureName(featureIndex);
-		String featureData = featureName+",";
-		featureData += getCardinalityOfFeatureAtIndex(featureIndex) + ",";
-		featureData += getModeDataOfCategoricalFeatureAtIndex(featureIndex);
-
-		return featureData;
-	}
-
-	private int getCardinalityOfFeatureAtIndex(int featureIndex) {
-		HashMap<Double, String> cardinalityMap =  new HashMap<Double, String>();
-		for (ArrayList<Double> dataInstance : dataInstances) {
-			cardinalityMap.put(dataInstance.get(featureIndex), "");
-		}
-		return cardinalityMap.size();
 	}
 
 	private String getQuartilesOfFeatureAtIndex(int featureIndex) {
@@ -183,25 +100,107 @@ public class DataProcessor {
 					}
 					firstMaxOccurrences = total;
 					firstMode = dataValue;
-				}
-				else if (total > secondMaxOccurrences && dataValue != firstMode) {
+				} else if (total > secondMaxOccurrences && dataValue != firstMode) {
 					secondMaxOccurrences = total;
 					secondMode = dataValue;
 				}
-			}
-			else {
+			} else {
 				modeMap.put(dataValue, 1);
 			}
 		}
 
 		if (firstMaxOccurrences == 1) {
 			return "N/A,N/A,N/A,N/A,N/A,N/A";
-		}
-		else if (secondMaxOccurrences == 1) {
+		} else if (secondMaxOccurrences == 1) {
 			return getNameFromIndexForCategoricalFeature(getFeatureName(featureIndex), (int)firstMode) + "," + modeMap.get(firstMode) + "," + (((double)modeMap.get(firstMode)/(double)this.dataInstances.size())*100) + "N/A,N/A,N/A";
 		}
 
 		return getNameFromIndexForCategoricalFeature(getFeatureName(featureIndex), (int)firstMode) + "," + modeMap.get(firstMode) + "," + (((double)modeMap.get(firstMode)/(double)this.dataInstances.size())*100) + "," + getNameFromIndexForCategoricalFeature(getFeatureName(featureIndex), (int)secondMode) + "," + modeMap.get(secondMode) + "," + (((double)modeMap.get(secondMode)/(double)this.dataInstances.size())*100);
+	}
+
+	private String processNumericFeatureData(int featureIndex) {
+		String featureName = getFeatureName(featureIndex);
+		String featureData = featureName+",";
+		featureData += getCardinalityOfFeatureAtIndex(featureIndex) + ",";
+		featureData += getQuartilesOfFeatureAtIndex(featureIndex) + ",";
+		featureData += getMeanAndStandardDeviationOfFeatureAtIndex(featureIndex);
+
+		return featureData;
+	}
+
+	private String processCategoricalFeatureData(int featureIndex) {
+		String featureName = getFeatureName(featureIndex);
+		String featureData = featureName+",";
+		featureData += getCardinalityOfFeatureAtIndex(featureIndex) + ",";
+		featureData += getModeDataOfCategoricalFeatureAtIndex(featureIndex);
+
+		return featureData;
+	}
+
+	public void addDataInstance(String dataInstance) {
+		String[] data = dataInstance.split(",");
+		ArrayList<Double> processedDataInstance = new ArrayList<Double>();
+		double processedValue = 0;
+		for (int i = 0; i < data.length; i++) {
+			if (getFeatureType(i) == "categorical") {
+				processedValue = getIndexOfValueForCategoricalFeature(getFeatureName(i), data[i]);
+				processedDataInstance.add(processedValue);
+			} else {
+				processedValue = Double.parseDouble(data[i]);
+				processedDataInstance.add(processedValue);
+			}
+		}
+		this.dataInstances.add(processedDataInstance);
+	}
+
+	public void parseDataFromArffFile() throws Exception {
+		try {
+			Scanner in = new Scanner(new File(inputFilename), "UTF-8");
+			in.nextLine();
+
+			while(in.hasNextLine()) {
+				String line = in.nextLine().trim();
+				if (line.length() > 0 && line.charAt(0) != '%' && !line.contains("@data") && !line.contains("@relation")) {
+					if (line.contains("@attribute")) {
+						String[] lineSplitOnSpace = line.split(" ");
+						switch (lineSplitOnSpace[2].trim()) {
+							case "numeric":
+								this.addFeature(lineSplitOnSpace[1].trim());
+								break;
+							default:
+								this.addFeature(lineSplitOnSpace[1].trim());
+								this.addCategoricalFeatureOptions(lineSplitOnSpace[1].trim(), lineSplitOnSpace[2].trim());
+								break;
+						}
+					} else {
+						this.addDataInstance(line);
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Issue reading in input file:" + e);
+			System.exit(0);
+		}
+	}
+
+	public void generateDataQualityReport() {
+		for (int i = 0; i < this.features.size(); i++) {
+			if (getFeatureType(i) == "numeric") {
+				dataPrinter.addNumericFeatureData(processNumericFeatureData(i));
+			} else if (getFeatureType(i) == "categorical") {
+				dataPrinter.addCategoricalFeatureData(processCategoricalFeatureData(i));
+			}
+		}
+
+		dataPrinter.setTotalNumberOfInstances(this.dataInstances.size());
+	}
+
+	public void setInputFilename(String filename) {
+		this.inputFilename = filename;
+	}
+
+	public void setDataPrinter(DataPrinter dataPrinter) {
+		this.dataPrinter = dataPrinter;
 	}
 
 }
